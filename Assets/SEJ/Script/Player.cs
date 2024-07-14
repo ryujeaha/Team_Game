b522160a35f72afd33bfebdbae9dbdfb64d054cc
xun-zx <songej0427@gmail.com>
2024-07-14 21:24:18 +0900

    24.07.14.
    송은제 자동완성오류 해결

--- Assets/SEJ/Script/Player.cs
+++ Assets/SEJ/Script/Player.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject;
+
     // Start is called before the first frame update
     void Start()
-    {
-    }
-
-    // Update is called once per frame
-   void Update()
     {
         
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
