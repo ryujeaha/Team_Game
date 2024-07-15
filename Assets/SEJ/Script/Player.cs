878ede00457a6c693a3e0e6fb9fe355b3f52caa3
xun-zx <songej0427@gmail.com>
2024-07-15 21:21:06 +0900

    24.07.15. 송은제 좌우이동 추가
    - Update 함수에 Input.GetKey 사용
    transform.Translate(new Vector2~~);
    Z축 필요 없다고 판단해서 Vector3 대신 Vector2 함수 사용
    - 움직이는 것 확인하려고 Sprite Renderer에 빨간색 기본 제공 이미지 넣어놓음.
    - 움직이는 속도는 배경화면과 이미지 만들고 조정해야 할듯?

--- Assets/SEJ/Script/Player.cs
+++ Assets/SEJ/Script/Player.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)) //키보드 A 누르는중일 시
+        {
+            Debug.Log("A키 누름");
+            transform.Translate(new Vector2(-0.006f, 0)); //Z축 이동 필요 없을 것 같아서 Vector2 사용
+        }
+
+        if (Input.GetKey(KeyCode.D)) //키보드 D 누르는중일 시
+        {
+            Debug.Log("D키 누름");
+            transform.Translate(new Vector2(0.006f, 0));
+        }
     }
 }
