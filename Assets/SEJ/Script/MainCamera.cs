d7d174414a8bfba9d9951c9834d8577aae7a209b
xun-zx <songej0427@gmail.com>
2024-07-21 14:16:17 +0900

    2024.07.21. Player 기준 카메라 따라가기
    카메라 속도 조절 가능.
    아이템 넘어갈지 아닐지 회의때 이야기

---
+++ Assets/SEJ/Script/MainCamera.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed; // 카메라가 따라갈 속도
+    private Vector3 targetPosition; // 대상의 현재 위치
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.gameObject != null)
+        {
+            // this는 카메라 의미
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
